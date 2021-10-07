42e9d9d24519fd38726936a3f1d480c43cb99bdb
kanedaaaa <samflynn19121@gmail.com>
2021-10-07 14:10:23 +0200

    latest update in staking logic

--- contracts/Masterdemon.sol
+++ contracts/Masterdemon.sol
@@ -15,46 +15,56 @@ contract MasterDemon is Ownable, ReentrancyGuard {
     struct UserInfo {
         uint256 amountStaked; // how many nfts did user staked
         uint256 currentRewad; // how much reward should user get
-        uint256[] tokenIds;   // ids of nfts user staked
-        mapping (uint256 => uint256) tokenIndex; // for delicate operations
+        uint256[] tokenIds; // ids of nfts user staked
+        mapping(uint256 => uint256) tokenIndex; // for delicate operations
+    }
+
+    struct NftCollection {
+        address collectionAddress;
+        uint256 stakingFee;
+        uint256 harvestingFee;
+        uint256 withdrawingFee;
+        uint256 normalizer;
+        uint256 multiplier;
     }
 
     /// @notice address => each user
     mapping(address => UserInfo) public userInfo;
 
+    NftCollection[] public nftCollection;
+
     /// @notice LLTH token
     IERC20 public llth;
-
-    /// @notice collection 
-    IERC721 public nftCollection;
 
     event UserStaked(address staker);
     event UserUnstaked(address unstaker);
     event UserHarvested(address harvester);
 
-    constructor (IERC20 _llth) public {
+    constructor(IERC20 _llth) public {
         llth = _llth;
     }
 
     // ------------------------ PUBLIC/EXTERNAL ------------------------ //
 
-    function stake(uint256 _id) external {
-        _stake(msg.sender, _id);
-    } 
-
-    function unstake(uint256 _id) external {
-        _unstake(msg.sender, _id);
+    function stake(uint256 _cid, uint256 _id) external {
+        _stake(msg.sender, _cid, _id);
     }
 
-    function stakeBatch(uint256[] memory _ids) external {
-        for (uint256 i=0; i<_ids.length; ++i) {
-            _stake(msg.sender, _ids[i]);
+    function unstake(uint256 _id, uint256 _cid) external {
+        _unstake(msg.sender, _cid, _id);
+    }
+
+    function stakeBatch(uint256 _collectionAddress, uint256 _cid, uint256[] memory _ids)
+        external
+    {
+        for (uint256 i = 0; i < _ids.length; ++i) {
+            _stake(msg.sender, _cid, _ids[i]);
         }
     }
 
-    function unstakeBatch(uint256[] memory _ids) external {
-        for (uint256 i=0; i<_ids.length; ++i) {
-            _unstake(msg.sender, _ids[i]);
+    function unstakeBatch(uint256[] memory _ids, uint256 _cid) external {
+        for (uint256 i = 0; i < _ids.length; ++i) {
+            _unstake(msg.sender, _cid, _ids[i]);
         }
     }
 
@@ -67,25 +77,28 @@ contract MasterDemon is Ownable, ReentrancyGuard {
     /// @notice stake single nft (called in external function)
     /// @param _user = msg.sender
     /// @param _id = nft id
-    function _stake(address _user, uint256 _id) internal {
-        require(IERC721(nftCollection).ownerOf(_id) == address(_user), "ERR: YOU DONT OWN THIS TOKEN");
-        IERC721(nftCollection).safeTransferFrom(_user, address(this), _id);
-
+    function _stake(address _user, uint256 _cid, uint256 _id) internal {
+        NftCollection memory collection = nftCollection[_cid];
         UserInfo storage user = userInfo[_user];
+        require(
+            IERC721(collection.collectionAddress).ownerOf(_id) == address(_user),
+            "ERR: YOU DONT OWN THIS TOKEN"
+        );
+        IERC721(collection.collectionAddress).safeTransferFrom(_user, address(this), _id);
         user.amountStaked = user.amountStaked.add(1);
         user.tokenIds.push(_id);
 
         emit UserStaked(_user);
-    } 
+    }
 
     /// @notice unstake single nft (called in external function)
     /// @param _user = msg.sender
     /// @param _id = nft id
-    function _unstake(address _user, uint256 _id) internal {
-        require(IERC721(nftCollection).ownerOf(_id) == address(_user), "ERR: YOU DONT OWN THIS TOKEN");
-
+    function _unstake(address _user, uint256 _cid, uint256 _id) internal {
+        NftCollection memory collection = nftCollection[_cid];
         UserInfo storage user = userInfo[_user];
-        IERC721(nftCollection).safeTransferFrom(address(this), _user, _id);
+        // massive security issue here, user can withdraw any token, should check if they really staked them!!!
+        IERC721(collection.collectionAddress).safeTransferFrom(address(this), _user, _id);
 
         uint256 lastIndex = user.tokenIds.length - 1;
         uint256 lastIndexKey = user.tokenIds[lastIndex];
@@ -97,14 +110,13 @@ contract MasterDemon is Ownable, ReentrancyGuard {
             user.tokenIds.pop();
             delete user.tokenIndex[_id];
             user.amountStaked -= 1;
-            }
+        }
 
         if (user.amountStaked == 0) {
             delete userInfo[msg.sender];
         }
 
         emit UserUnstaked(_user);
-
     }
 
     function _harvest(address _user) internal {
@@ -120,30 +132,27 @@ contract MasterDemon is Ownable, ReentrancyGuard {
     /// @notice dummy function, will be replaced by oracle later
     function _getRarity() internal pure returns (uint256) {
         return 40;
-    } 
+    }
 
     /// @notice will calculate rarity based on our formula
     /// @param _rarity number given my trait normalization formula
     /// @param _normalizer number that will range _rarity into normalized numbers range
-    /// @param _daysStaked maturity period 
+    /// @param _daysStaked maturity period
     /// @param _multiplier pool can have multiplier
-    /// @param _amountOfStakers used to minimize rewards proportionally to pool popularity 
+    /// @param _amountOfStakers used to minimize rewards proportionally to pool popularity
     function _calculateRewards(
-        uint256 _rarity, 
+        uint256 _rarity,
         uint256 _normalizer,
-        uint256 _daysStaked, 
-        uint256 _multiplier, 
+        uint256 _daysStaked,
+        uint256 _multiplier,
         uint256 _amountOfStakers
-    )
-        internal returns (uint256)
-    {
-
+    ) internal returns (uint256) {
         require(
-            _rarity != 0 && 
-            _daysStaked !=0 && 
-            _multiplier !=0 && 
-            _amountOfStakers !=0 &&
-            _normalizer !=0, 
+            _rarity != 0 &&
+                _daysStaked != 0 &&
+                _multiplier != 0 &&
+                _amountOfStakers != 0 &&
+                _normalizer != 0,
             "CANT BE ZERO"
         );
 
@@ -154,7 +163,27 @@ contract MasterDemon is Ownable, ReentrancyGuard {
 
         return finalReward;
     }
-    
+
+    // ------------------------ ADMIN ------------------------ //
+
+    function setCollection(
+        address _collectionAddress,
+        uint256 _stakingFee,
+        uint256 _harvestingFee,
+        uint256 _withdrawingFee,
+        uint256 _normalizer,
+        uint256 _multiplier
+    ) public onlyOwner {
+        nftCollection.push(NftCollection({
+            collectionAddress: _collectionAddress,
+            stakingFee: _stakingFee,
+            harvestingFee: _harvestingFee,
+            withdrawingFee: _withdrawingFee,
+            normalizer: _normalizer,
+            multiplier: _multiplier
+        }));
+    }
+
     function onERC721Received(
         address,
         address,
