934aa6a2ed7ace582d94b931dbe851ceb920497d
Max <leonboripgs0818@outlook.com>
2021-10-15 11:50:23 +0900

    add test code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 const Masterdemon = artifacts.require("Masterdemon");
-const LLTH = artifacts.require("LLTH");
-const Collection = artifacts.require("Collection");
+const MockLLTH = artifacts.require("MockLLTH");
+const MockCollection = artifacts.require("MockCollection");
 
 module.exports = async (deployer) => {
-    await deployer.deploy(Collection);
+    await deployer.deploy(MockCollection);
 
-    await deployer.deploy(LLTH)
+    await deployer.deploy(MockLLTH)
         .then((pre) => deployer.deploy(Masterdemon, pre.address))
 }
