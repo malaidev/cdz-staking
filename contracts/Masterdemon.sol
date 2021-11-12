edba45208ba5f63508c90041b7099b74a664174b
kanedaaaa <samflynn19121@gmail.com>
2021-11-12 03:10:51 +0100

    fix array

--- contracts/Masterdemon.sol
+++ contracts/Masterdemon.sol
@@ -180,7 +180,7 @@ contract Masterdemon is Ownable, ReentrancyGuard, usingProvable {
         uint256 _id
     ) internal {
         UserInfo storage user = userInfo[_user];
-        CollectionInfo memory collection = collectionInfo[_cid];
+        CollectionInfo storage collection = collectionInfo[_cid];
 
         require(
             user.stakedTokens[collection.collectionAddress].length <
@@ -194,7 +194,7 @@ contract Masterdemon is Ownable, ReentrancyGuard, usingProvable {
             _id
         );
 
-        if (user.stakedTokens[collection.collectionAddress].length == 0) { // potential issue here where if statement isn't triggering %%%%%%%%%%%%%%%%%%%%%%%%%%
+        if (user.stakedTokens[collection.collectionAddress].length == 0) { 
             collection.amountOfStakers += 1;
         }
 
@@ -226,7 +226,7 @@ contract Masterdemon is Ownable, ReentrancyGuard, usingProvable {
         uint256 _id
     ) internal {
         UserInfo storage user = userInfo[_user];
-        CollectionInfo memory collection = collectionInfo[_cid];
+        CollectionInfo storage collection = collectionInfo[_cid];
 
         require(
             tokenOwners[collection.collectionAddress][_id] == _user,
@@ -241,6 +241,10 @@ contract Masterdemon is Ownable, ReentrancyGuard, usingProvable {
         );
 
         user.stakedTokens[collection.collectionAddress].removeElement(_id);
+
+        if (user.stakedTokens[collection.collectionAddress].length == 0) {
+            collection.amountOfStakers -= 1;
+        }
         
         // delete will leave 0x000...000
         delete tokenOwners[collection.collectionAddress][_id];
@@ -248,9 +252,9 @@ contract Masterdemon is Ownable, ReentrancyGuard, usingProvable {
         user.stakedTimestamp = 0;
         user.amountStaked -= 1;
 
-        if (user.amountStaked == 0) {
-            delete userInfo[_user];
-        }
+        //if (user.amountStaked == 0) {
+        //    delete userInfo[_user];
+        //}
     }
 
     /**
@@ -422,7 +426,7 @@ contract Masterdemon is Ownable, ReentrancyGuard, usingProvable {
                 harvestingFee: _harvestingFee,
                 multiplier: _multiplier,
                 maturityPeriod: _maturityPeriod,
-                amountOfStakers: 1, // notice, its for testing purposes. this should be 0 in production %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+                amountOfStakers: 0, // notice, its for testing purposes. this should be 0 in production %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
                 stakingLimit: _stakingLimit
             })
         );
@@ -488,14 +492,14 @@ contract Masterdemon is Ownable, ReentrancyGuard, usingProvable {
         provable_setCustomGasPrice(_newGasPrice);
     }
 
-    function getUser(address _user, address _collection) public view returns (uint256, uint256, uint256, uint256[] memory){
+    function getUser(address _user, address _collection) public view returns (uint256){
         UserInfo storage user = userInfo[_user];
-        uint256 amountStaked = user.amountStaked;
-        uint256 daysStaked = (block.timestamp - user.stakedTimestamp) / 86400;
-        uint256 userBalance = user.userBalance;
-        uint256[] memory stakedTokens = user.stakedTokens[_collection];
+        return user.amountStaked;
+    }
 
-        return (amountStaked, daysStaked, userBalance, stakedTokens);
+    function getCollectionInfo(uint256 _cid) public view returns (uint256) {
+        CollectionInfo memory collection = collectionInfo[_cid];
+        return collection.amountOfStakers;
     }
     
 
