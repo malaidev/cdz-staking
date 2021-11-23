6922150aa343d5fec0b1689df3cfc6aff4ff2546
kisile <elisikmusic@gmail.com>
2021-11-23 21:57:22 +0000

    Unit testing Chainlink oracle calls  with Truffle sucks

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,7 @@ const MockLLTH = artifacts.require("MockLLTH");
 const MockCollection = artifacts.require("MockCollection");
 const ArrayLib = artifacts.require("Array");
 
-const Harvest = artifacts.require("Harvest"); 
+const Harvester = artifacts.require("Harvester"); 
 
 
 
@@ -13,7 +13,7 @@ module.exports = async (deployer) => {
     await deployer.link(ArrayLib, [Masterdemon]);
     await deployer.deploy(MockLLTH).then(res => {
         deployer.deploy(Masterdemon, res.address)
-        deployer.deploy(Harvest, res.address);
+        deployer.deploy(Harvester, res.address);
     })
     await deployer.deploy(MockCollection);
 
