64a871ff56f8dde5274dc6379ef50291c11df5bb
kanedaaaa <samflynn19121@gmail.com>
2021-11-27 12:33:42 +0100

    sorry not sorry

--- truffle-config.js
+++ truffle-config.js
@@ -45,47 +45,47 @@ module.exports = {
     // tab if you use this network and you must also set the `host`, `port` and `network_id`
     // options below to some value.
     //
-  //   development: {
-  //     host: "127.0.0.1",     // Localhost (default: none)
-  //     port: 9545,            // Standard Ethereum port (default: none)
-  //     network_id: "*"      // Any network (default: none)
-  //   },
-  //   // Another network with more advanced options...
-  //   // advanced: {
-  //   // port: 8777,             // Custom port
-  //   // network_id: 1342,       // Custom network
-  //   // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
-  //   // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
-  //   // from: <address>,        // Account to send txs from (default: accounts[0])
-  //   // websocket: true        // Enable EventEmitter interface for web3 (default: false)
-  //   // },
-  //   // Useful for deploying to a public network.
-  //   // NB: It's important to wrap the provider as a function.
-  //   mumbai: {
-  //     provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://polygon-mumbai.infura.io/v3/" + process.env.INFURA_KEY),
-  //     network_id: 80001,       // Ropsten's id
-  //     gas: 5500000,        // Ropsten has a lower block limit than mainnet
-  //     confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-  //     timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-  //     skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-  //   },
-  //   // Useful for private networks
-  //   // private: {
-  //   // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
-  //   // network_id: 2111,   // This network is yours, in the cloud.
-  //   // production: true    // Treats this network as if it was a public net. (default: false)
-  //   // }
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 9545,            // Standard Ethereum port (default: none)
+      network_id: "*"      // Any network (default: none)
+    },
+    // Another network with more advanced options...
+    // advanced: {
+    // port: 8777,             // Custom port
+    // network_id: 1342,       // Custom network
+    // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    // from: <address>,        // Account to send txs from (default: accounts[0])
+    // websocket: true        // Enable EventEmitter interface for web3 (default: false)
+    // },
+    // Useful for deploying to a public network.
+    // NB: It's important to wrap the provider as a function.
+    mumbai: {
+      provider: () => new HDWalletProvider(process.env.MNEMONIC, "https://polygon-mumbai.infura.io/v3/" + process.env.INFURA_KEY),
+      network_id: 80001,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    // Useful for private networks
+    // private: {
+    // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
+    // network_id: 2111,   // This network is yours, in the cloud.
+    // production: true    // Treats this network as if it was a public net. (default: false)
+    // }
 
-  //   // using bsc testnet because all ethereum testnet faucets are broken
-  //   testnet: {
-  //     provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s2.binance.org:8545`),
-  //     network_id: 97,
-  //     confirmations: 10,
-  //     timeoutBlocks: 200,
-  //     networkCheckTimeout: 100000000,
-  //     skipDryRun: true
-  //   }
-  // },
+    // using bsc testnet because all ethereum testnet faucets are broken
+    // testnet: {
+    //   provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s2.binance.org:8545`),
+    //   network_id: 97,
+    //   confirmations: 10,
+    //   timeoutBlocks: 200,
+    //   networkCheckTimeout: 100000000,
+    //   skipDryRun: true
+    // }
+  },
 
   // Set default mocha options here, use special reporters etc.
   mocha: {
